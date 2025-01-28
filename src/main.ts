f7b9cf81150d444ca3f075edbe4efbf88eaf7a7f
marcelaoliveira2 <163016115+marcelaoliveira2@users.noreply.github.com>
2025-01-28 07:08:31 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,51 +2,63 @@ import express from 'express'
 import cors from 'cors'
 import mysql from 'mysql2/promise'
 
+
 const app = express()
+
 
 app.use(express.json())
 
+
 app.use(cors())
 
-import BancoMysql from './db/bancoMysql'
 
-app.delete("/produtos/:id",async (req,res)=>{
-    console.log("Tentando excluir o produto de id:",req.params.id)
+app.get("/produtos",async(req,res)=>{
+
+
     try{
-        const sqlQuery = "DELETE FROM produtos WHERE id = ?"
-        const parametro = [req.params.id]
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"catalogo_cosmeticos",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+       
+        const [result,fields]  = await conexao.query("SELECT * FROM cosmeticos")
+        await conexao.end()
 
-        const banco = new BancoMysql();
 
-        const result = await banco.excluir(req.params.id)
+        res.send(result)
+    }catch(e){
+        console.log(e)
+        res.status(500).send("Erro do servidor")
+    }  
+})
 
+
+app.post("/produtos",async(req,res)=>{
+    try{
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"catalogo_cosmeticos",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        const {id,nome,descricao,valor,imagem} = req.body
+        const [result,fields]  =
+            await conexao.query("INSERT INTO cosmeticos VALUES (?,?,?,?,?)",
+                [id,nome,descricao,valor,imagem])
+        await conexao.end()
         res.status(200).send(result)
     }catch(e){
         console.log(e)
         res.status(500).send("Erro do servidor")
-    }
-})
-app.put("/produtos/:id",async (req,res)=>{
-    console.log("Tentando alterar o produto de id:",req.params.id)
-    try{
-        const {nome,descricao,valor,imagem} = req.body
-        //const sqlQuery = "UPDATE produtos SET nome=?,descricao=?,preco=?,imagem=? WHERE id = ?"
-        const produto = {nome,descricao,valor,imagem}
-
-        const banco = new BancoMysql();
-
-        const result = await banco.alterar(req.params.id,produto)
-
-        res.status(200).send(result)
-    }catch(e){
-        console.log(e)
-        res.status(500).send("Erro do servidor")
-    }
+    }  
 })
 
 
-
-//INICIAR O SERVIDOR
 app.listen(8000,()=>{
     console.log("SERVIDOR INICIADO NA PORTA 8000")
 })
+
