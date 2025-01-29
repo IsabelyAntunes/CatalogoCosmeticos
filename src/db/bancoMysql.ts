d2a0780ebc7c319782f77e97058d4457cf1ea495
marcelaoliveira2 <163016115+marcelaoliveira2@users.noreply.github.com>
2025-01-29 09:12:11 -0400

    .

--- src/db/bancoMysql.ts
+++ src/db/bancoMysql.ts
@@ -1,28 +1,45 @@
 import mysql, { Connection } from 'mysql2/promise';
 
 class BancoMysql {
+    listarPorId(id: string) {
+        throw new Error('Method not implemented.');
+    }
+    // Propriedade
     private conexao: Promise<Connection>;
 
-    constructor(){
+    // Métodos
+    constructor() {
         this.conexao = mysql.createConnection({
-            host: process.env.dbhost?process.env.dbhost:"localhost",
-            user:process.env.dbuser?process.env.dbuser:"root",
-            password:process.env.dbpassword?process.env.dbpassword:"",
-            database:process.env.dbname?process.env.dbname:"catalogo_cosmeticos",
-            port:process.env.dbport?parseInt(process.env.dbport):3306
+            host: process.env.dbhost ? process.env.dbhost : "localhost",
+            user: process.env.dbuser ? process.env.dbuser : "root",
+            password: process.env.dbpassword ? process.env.dbpassword : "",
+            database: process.env.dbname ? process.env.dbname : "banco1022b",
+            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
         });
     }
 
-    async getConnection(){
-        const conn = await this.conexao;
+    async getConnection() {
+        const conn = await this.conexao; 
         return conn;
     }
 
-    async end(){
-        const conn = await this.conexao;
+    async end() {
+        const conn = await this.conexao; 
         await conn.end();
     }
 
+    async listar(){
+        const conn = await this.getConnection()
+        const [result, fields] = await conn.query("SELECT * from produtos");
+        return result
+    }
+    async inserir(produto:{id:string,nome:string,descricao:string,valor:string,imagem:string}){
+        const conn = await this.getConnection()
+        const sqlQuery = "INSERT INTO produtos (id,nome,descricao,preco,imagem) VALUES (?,?,?,?,?)"
+        const parametro = [produto.id,produto.nome,produto.descricao,produto.valor,produto.imagem]
+        const [result, fields] = await conn.query(sqlQuery,parametro);
+        return result
+    }
     async excluir(id:string){
         const conn = await this.getConnection()
         const sqlQuery = "DELETE FROM produtos WHERE id = ?"
@@ -30,10 +47,9 @@ class BancoMysql {
         const [result, fields] = await conn.query(sqlQuery,parametro);
         return result
     }
-
     async alterar(id:string,produto:{id?:string,nome:string,descricao:string,valor:string,imagem:string}){
         const conn = await this.getConnection()
-        const sqlQuery = "UPDATE produtos SET nome=?,descricao=?,valor=?,imagem=? WHERE id = ?"
+        const sqlQuery = "UPDATE produtos SET nome=?,descricao=?,preco=?,imagem=? WHERE id = ?"
         const parametro = [produto.nome,produto.descricao,produto.valor,produto.imagem,id]
         const [result, fields] = await conn.query(sqlQuery,parametro);
         return result
